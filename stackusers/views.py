0d60b56e74808546a4d8e4d1cbb95a0e4c1fc0ff
SeyiAyo <oluwaseyiayoola97@gmail.com>
2023-01-07 12:05:56 +0100

    temp registration redirect & reg database set

--- stackusers/views.py
+++ stackusers/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'register.html', {'form' : form})
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account {username} succesfully Created Log In Now')
+            return redirect('stackbase:home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form' : form})   
