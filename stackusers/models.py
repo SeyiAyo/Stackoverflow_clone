7bf5c7da31b5232dd901b06b8845792160b5268b
SeyiAyo <oluwaseyiayoola97@gmail.com>
2023-01-14 22:44:38 +0100

    Profile pic img database update

--- stackusers/models.py
+++ stackusers/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    User = models.OneToOneField(User, on_delete = models.CASCADE)
+    bio = models.CharField(max_length = 1000)
+    Phone = models.IntegerField()
+    Image = models.ImageField(default = 'default.jpg', upload_to = "profile_pic")
+
+
+    def __str__(self):
+        return f'{self.User.username} - Profile'
